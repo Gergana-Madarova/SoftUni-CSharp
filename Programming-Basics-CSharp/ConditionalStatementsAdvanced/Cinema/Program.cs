0a929a24e9f3b855382de438f680bd4c18c5cf27
Gergana-Madarova <g.madarova@gmail.com>
2021-01-07 17:46:00 +0200

    Added Conditional Statements Advanced - Cinema

---
+++ Programming-Basics-CSharp/ConditionalStatementsAdvanced/Cinema/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+
+            double outPut = 0.0;
+
+            switch (type)
+            {
+                case "Premiere":
+                    outPut = 12 * rows * columns;
+                    break;
+                case "Normal":
+                    outPut = 7.50 * rows * columns;
+                    break;
+                case "Discount":
+                    outPut = 5.00 * rows * columns;
+                    break;
+            }
+
+            Console.WriteLine($"{outPut:f2} leva");
+        }
+    }
+}
