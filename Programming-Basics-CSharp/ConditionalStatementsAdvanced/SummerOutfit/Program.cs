706bc949e7fbe8ec3248ed81b96ed402362d4fb9
Gergana-Madarova <g.madarova@gmail.com>
2021-01-08 16:57:10 +0200

    Revert "Added Summer Outfit to ConditionalStatementsAdvanced"
    This reverts commit eb992d56250dcd4543995bc17dd68d11fcf57bb9.

---
+++ Programming-Basics-CSharp/ConditionalStatementsAdvanced/SummerOutfit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SummerOutfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degrees = int.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+
+            string outfit = string.Empty;
+            string shoes = string.Empty;
+
+            if ((timeOfDay == "Evening") || ((timeOfDay == "Afternoon") && (degrees <= 18 && degrees >= 10)) || ((timeOfDay == "Morning") && (degrees <= 24 && degrees > 18)))
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+            else if (((timeOfDay == "Afternoon") && (degrees <= 24 && degrees > 18)) || ((timeOfDay == "Morning") && (degrees >= 25)))
+            {
+                outfit = "T-Shirt";
+                shoes = "Sandals";
+            }
+            else if ((timeOfDay == "Morning") && (degrees <= 18 && degrees >= 10))
+            {
+                outfit = "Sweatshirt";
+                shoes = "Sneakers";
+            }
+            else if ((timeOfDay == "Afternoon") && (degrees >= 25))
+            {
+                outfit = "Swim Suit";
+                shoes = "Barefoot";
+            }
+
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
