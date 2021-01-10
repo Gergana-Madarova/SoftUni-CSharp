8c3519e441531e127d3530e98456a831c1103f75
Gergana-Madarova <g.madarova@gmail.com>
2021-01-11 00:21:46 +0200

    Added Volleyball to ConditionalStatementsAdvanced

---
+++ Programming-Basics-CSharp/ConditionalStatementsAdvanced/Volleyball/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"leap" (високосна година) или "normal" (невисокосна)
+            string year = Console.ReadLine();
+            //брой празници в годината (които не са събота и неделя)
+            double p = int.Parse(Console.ReadLine());
+            //брой уикенди, в които пътува до родния град
+            double h = int.Parse(Console.ReadLine());
+            //колко пъти Влади е играл волейбол през годината. 
+            double count = 0.0;
+
+            count = h + ((48 - h) * 3 / 4) + (2 * p / 3);
+            if (year == "leap")
+            {
+                count = count + 0.15 * count;
+            }
+
+            //Закръглете резултата надолу до най-близкото цяло число
+            Console.WriteLine($"{Math.Floor(count)}");
+        }
+    }
+}
