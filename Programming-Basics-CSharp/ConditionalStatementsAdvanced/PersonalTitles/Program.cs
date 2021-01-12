1a6fd00c4dbf20ae0868620eea3e1415e3b5af3b
Gergana-Madarova <g.madarova@gmail.com>
2021-01-12 20:38:06 +0200

    Added PersonalTitles in ConditionalStatementsAdvanced

---
+++ Programming-Basics-CSharp/ConditionalStatementsAdvanced/PersonalTitles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PersonalTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+            string outPut = string.Empty;
+
+            switch (gender)
+            {
+                case "f":
+                    if (age >= 16)
+                    {
+                        outPut = "Ms.";
+                    }
+                    else
+                        outPut = "Miss";
+                    break;
+
+                case "m":
+                    if (age >= 16)
+                    {
+                        outPut = "Mr.";
+                    }
+                    else
+                        outPut = "Master";
+                    break;
+            }
+
+            Console.WriteLine(outPut);
+        }
+    }
+}
