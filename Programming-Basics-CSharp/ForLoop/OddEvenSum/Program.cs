48c3ff50e4cd06a55c08e4851d89cba21819ab20
Gergana-Madarova <g.madarova@gmail.com>
2021-01-26 23:32:41 +0200

    Added OddEvenSum to ForLoop

---
+++ Programming-Basics-CSharp/ForLoop/OddEvenSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum1 = 0;
+            int sum2 = 0;
+            int diff = 0;
+            int number = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                if (i % 2 != 0)
+                {
+                    sum1 +=number;
+                }
+                else if (i % 2 == 0)
+                {
+                    sum2 += number;
+                }
+            }
+
+            if (sum1==sum2)
+            {
+                Console.WriteLine($"Yes\nSum = {sum1}");
+            }
+            else
+            {
+                diff = Math.Abs(sum1 - sum2);
+                Console.WriteLine($"No\nDiff = {diff}");
+            }
+        }
+    }
+}
