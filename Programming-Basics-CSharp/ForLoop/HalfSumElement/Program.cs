f016420f097f31776bcb25d62994aa37a461e91c
Gergana-Madarova <g.madarova@gmail.com>
2021-01-26 23:31:33 +0200

    Added HalfSumElement to ForLoop

---
+++ Programming-Basics-CSharp/ForLoop/HalfSumElement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = int.MinValue;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number > max)
+                {
+                    max = number;
+                }
+                sum += number;
+            }
+
+            if ((sum-max) == max)
+            {
+                Console.WriteLine("Yes\nSum = " + max);
+            }
+            else
+            {
+                int diff = Math.Abs(max - (sum - max));
+                Console.WriteLine("No\nDiff = " + diff);
+            }
+        }
+    }
+}
