9a691aea91bc440a9f4848f338207d28a87bcb50
Gergana-Madarova <g.madarova@gmail.com>
2021-01-28 13:28:26 +0200

    Added ReadText and Password in WhileLoop

---
+++ Programming-Basics-CSharp/WhileLoop/Password/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            string pass = Console.ReadLine();
+
+            while(pass != password)
+            {
+                pass = Console.ReadLine();
+            }
+            Console.WriteLine($"Welcome {user}!");
+        }
+    }
+}
