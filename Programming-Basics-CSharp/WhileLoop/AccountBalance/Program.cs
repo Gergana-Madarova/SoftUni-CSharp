6dad9ec083ccb8c43ad19ea6b32d7439c1a49a29
Gergana-Madarova <g.madarova@gmail.com>
2021-01-28 13:34:28 +0200

    Added AccountBalance and MaxNumber in WhileLoop

---
+++ Programming-Basics-CSharp/WhileLoop/AccountBalance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string money = Console.ReadLine();
+
+            double total = 0.0;
+
+            while (money != "NoMoreMoney")
+            {
+                if (double.Parse(money) < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                else
+                {
+                    total += double.Parse(money);
+                    Console.WriteLine($"Increase: {double.Parse(money):f2}");
+                }
+                money = Console.ReadLine();
+            }
+                Console.WriteLine($"Total: {total:f2}");
+        }
+    }
+}
---
+++ Programming-Basics-CSharp/WhileLoop/MaxNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int max = int.MinValue;
+            int curentNumber = 0;
+
+            while (number != "Stop")
+            {
+                curentNumber = int.Parse(number);
+                if (curentNumber > max)
+                {
+                    max = curentNumber;
+                }
+
+                number = Console.ReadLine();
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
