f64de87ce4549c3da0985a90523b86308598a5c2
Gergana-Madarova <g.madarova@gmail.com>
2021-02-03 17:14:14 +0200

    Added Graduation2 in WhileLoop

---
+++ Programming-Basics-CSharp/WhileLoop/Graduation2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Graduation2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double averageGrade = double.Parse(Console.ReadLine());
+            double averageGradeTotal = 0.0;
+            int k = 0;
+            bool excluded = false;
+
+            for (int i = 1; i <= 12; i++)
+            {
+                if (averageGrade < 4.00 && excluded == false)
+                {
+                    excluded = true;
+                    continue;
+                }
+                else if ((averageGrade < 4.00) && excluded == true)
+                {
+                    k = i-1;
+                    break;
+                }
+                else
+                {
+                    averageGradeTotal += averageGrade;
+                    if (i == 12)
+                    {
+                        break;
+                    }
+                    averageGrade = double.Parse(Console.ReadLine());
+                }
+
+            }
+
+            if (excluded == false)
+            {
+               // Console.WriteLine(averageGradeTotal);
+                Console.WriteLine($"{name} graduated. Average grade: {averageGradeTotal / 12:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {k} grade");
+            }
+
+            //while (averageGrade>=4.00)
+            //{
+
+
+            //}
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
