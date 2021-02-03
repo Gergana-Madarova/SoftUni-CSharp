905484b8de99e9e1b0963c723f2ff7d18922571c
Gergana-Madarova <g.madarova@gmail.com>
2021-02-03 17:12:13 +0200

    Added MinNumber in WhileLoop

---
+++ Programming-Basics-CSharp/WhileLoop/MinNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int min = int.MaxValue;
+            int curentNumber = 0;
+
+            while (number != "Stop")
+            {
+                curentNumber = int.Parse(number);
+                if (curentNumber < min)
+                {
+                    min = curentNumber;
+                }
+
+                number = Console.ReadLine();
+            }
+
+            Console.WriteLine(min);
+        }
+    }
+}
