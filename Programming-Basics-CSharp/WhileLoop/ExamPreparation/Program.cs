05b92e32c4e8b1d49df276fc3dbfabe108f98fbb
Gergana-Madarova <g.madarova@gmail.com>
2021-02-04 10:31:27 +0200

    Added OldBooks and ExamPreparation in WhileLoop

---
+++ Programming-Basics-CSharp/WhileLoop/ExamPreparation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfFailed = int.Parse(Console.ReadLine());
+            string problem = string.Empty;
+            int score = 0;
+            double sumScore = 0.0;
+            int p = 0;
+            int curNumOfFailed = 1;
+            bool failed = false;
+            string lastProblem = string.Empty;
+
+            while (true)
+            {
+                problem = Console.ReadLine();
+
+                if (problem == "Enough")
+                {
+                    break;
+                }
+
+                score = int.Parse(Console.ReadLine());
+
+                if (score <= 4 && numOfFailed > curNumOfFailed)
+                {
+                    p++;
+                    sumScore += score;
+                    curNumOfFailed++;
+                    continue;
+                }
+                else if (score <= 4 && numOfFailed <= curNumOfFailed)
+                {
+                    failed = true;
+                    break;
+                }
+                else
+                {
+                    p++;
+                    sumScore += score;
+                    lastProblem = problem;
+                }
+            }
+
+            if (failed)
+            {
+                Console.WriteLine($"You need a break, {numOfFailed} poor grades.");
+            }
+            else
+            {
+                Console.WriteLine($"Average score: {sumScore / p:f2}\n" +
+                                  $"Number of problems: {p}\n" +
+                                  $"Last problem: {lastProblem}");
+            }
+        }
+    }
+}
---
+++ Programming-Basics-CSharp/WhileLoop/OldBooks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string desiredBook = Console.ReadLine();
+            string book = Console.ReadLine();
+            int i = 0;
+
+            while (book != desiredBook)
+            {
+                if (book == "No More Books")
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {i} books.");
+                    break;
+                }
+                else
+                {
+                    book = Console.ReadLine();
+                    i++;
+                }
+            }
+
+            if (book == desiredBook)
+            {
+                Console.WriteLine($"You checked {i} books and found it.");
+            }
+        }
+    }
+}
