8f9842650b25bf86e3b06c3d321e768f1e4a899a
Gergana-Madarova <g.madarova@gmail.com>
2021-02-12 03:29:30 +0200

    Added Building in NestedLoops

---
+++ Programming-Basics-CSharp/NestedLoops/Building/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counterFloor = int.Parse(Console.ReadLine());
+            int counterRoom = int.Parse(Console.ReadLine());
+
+            for (int i = counterFloor; i >= 1; i--)
+            {
+                if (counterFloor == i)
+                {
+                    for (int j = 0; j < counterRoom; j++)
+                    {
+                        Console.Write(($"L{i}{j} "));
+                    }
+                    Console.WriteLine();
+                }
+                else if (i % 2 == 1)
+                {
+                    for (int j = 0; j < counterRoom; j++)
+                    {
+                        Console.Write($"A{i}{j} ");
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for (int j = 0; j < counterRoom; j++)
+                    {
+                        Console.Write($"O{i}{j} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
