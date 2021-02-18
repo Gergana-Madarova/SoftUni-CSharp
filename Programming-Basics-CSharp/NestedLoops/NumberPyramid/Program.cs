f085efaac2c3e97f0e0435e3ca13d1ead23feb3f
Gergana-Madarova <g.madarova@gmail.com>
2021-02-18 23:03:22 +0200

    Added NumberPyramid in NestedLoops

---
+++ Programming-Basics-CSharp/NestedLoops/NumberPyramid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int current = 1;
+            bool bigger = false;
+
+            for (int rows = 1; rows <= number; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    if (current>number)
+                    {
+                        bigger = true;
+                        break;
+                    }
+                    Console.Write(current + " ");
+                    current++;
+                }
+                if (bigger)
+                {
+                    break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
