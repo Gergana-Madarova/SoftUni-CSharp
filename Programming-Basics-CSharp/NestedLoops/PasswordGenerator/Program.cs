756dbd6cb97dd177a32b7db655278f5f4c65baf6
Gergana-Madarova <g.madarova@gmail.com>
2021-02-18 23:04:45 +0200

    Added PasswordGenerator in NestedLoops

---
+++ Programming-Basics-CSharp/NestedLoops/PasswordGenerator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    for (char k = 'a'; k < (char)l + 97; k++)
+                    {
+                        for (char m = 'a'; m < (char)l + 97; m++)
+                        {
+                            int p = 0;
+                            if (i >= j)
+                            {
+                                p = i + 1;
+                            }
+                            else if (j > i)
+                            {
+                                p = j + 1;
+                            }
+
+                            while (p <= n)
+                            {
+                                string pass = i.ToString() + j + k + m + p;
+                                Console.Write(pass + " ");
+                                p++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //Console.Write((char)(l + 97));
+    }
+}
