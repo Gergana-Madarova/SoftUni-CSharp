349de30e1a527add099e498e78b71e1249dd3295
Gergana-Madarova <g.madarova@gmail.com>
2021-02-18 23:01:42 +0200

    Added EqualSumsEvenOddPosition in NestedLoops

---
+++ Programming-Basics-CSharp/NestedLoops/EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EqualSumsEvenOddPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNum = int.Parse(Console.ReadLine());
+            int maxNum = int.Parse(Console.ReadLine());
+
+            for (int i = minNum; i <= maxNum; i++)
+            {
+                string currentNum = i.ToString();
+                int oddSum = 0;
+                int evenSum = 0;
+
+                for (int j = 0; j < currentNum.Length; j++)
+                {
+                    int currentDigit = int.Parse(currentNum[j].ToString());
+                    if (j % 2 == 0)
+                    {
+                        evenSum += currentDigit;
+                    }
+                    else
+                    {
+                        oddSum += currentDigit;
+                    }
+                }
+
+                if (oddSum == evenSum)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
