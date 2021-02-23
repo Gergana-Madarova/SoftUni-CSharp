e6be1b71fc0a67b2a4cb042ccca58343bc9086b6
Gergana-Madarova <g.madarova@gmail.com>
2021-02-23 14:19:35 +0200

    Added TheatrePromotion in Basic Syntax, Conditional Statements and Loops

---
+++ Fundamentals CSharp/Basic Syntax, Conditional Statements and Loops/TheatrePromotion/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace TheatrePromotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfDay = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            string output = "Error!";
+
+            if (typeOfDay == "Weekday")
+            {
+                if ((0 <= age && age <= 18) || (64 < age && age <= 122))
+                {
+                    output = "12$";
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    output = "18$";
+                }
+            }
+
+            if (typeOfDay == "Weekend")
+            {
+                if ((0 <= age && age <= 18) || (64 < age && age <= 122))
+                {
+                    output = "15$";
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    output = "20$";
+                }
+            }
+
+            if (typeOfDay == "Holiday")
+            {
+                if (0 <= age && age <= 18)
+                {
+                    output = "5$";
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    output = "12$";
+                }
+                else if (64 < age && age <= 122)
+                {
+                    output = "10$";
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
