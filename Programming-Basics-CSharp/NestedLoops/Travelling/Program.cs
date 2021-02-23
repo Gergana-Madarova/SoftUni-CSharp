27c0139cf63db31db1afb80ebed0fae02c167112
Gergana-Madarova <g.madarova@gmail.com>
2021-02-23 14:04:42 +0200

    Added Travelling in NestedLoops

---
+++ Programming-Basics-CSharp/NestedLoops/Travelling/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            double sum = 0;
+
+            while (destination != "End")
+            {
+                double minMoney = double.Parse(Console.ReadLine());
+                do
+                {
+                    string saveMoney = Console.ReadLine();
+                    if (saveMoney != "End")
+                    {
+                        sum += double.Parse(saveMoney);
+                    }
+                    else
+                    {
+                        destination = "End";
+                        break;
+                    }
+                }
+                while (sum < minMoney);
+
+                if (destination == "End")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Going to {destination}!");
+                    sum = 0;
+                    destination = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
