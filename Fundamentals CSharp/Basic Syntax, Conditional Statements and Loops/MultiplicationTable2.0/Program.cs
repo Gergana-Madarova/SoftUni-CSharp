6b6dfef455575e8c78685524c83f48a7105d35ca
Gergana-Madarova <g.madarova@gmail.com>
2021-03-10 16:38:32 +0200

    Added MultiplicationTable2.0

---
+++ Fundamentals CSharp/Basic Syntax, Conditional Statements and Loops/MultiplicationTable2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+
+            string output = string.Empty;
+
+            if (times > 10)
+            {
+                Console.WriteLine($"{num} X {times} = {num * times}");
+            }
+
+            for (int i = times; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} X {i} = {num * i}");
+            }
+        }
+    }
+}
