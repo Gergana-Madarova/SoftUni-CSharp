c5efabafb4418a9a40785ee726e43fbd7ba87808
Gergana-Madarova <g.madarova@gmail.com>
2021-03-10 16:36:12 +0200

    Added ForeignLanguages

---
+++ Fundamentals CSharp/Basic Syntax, Conditional Statements and Loops/ForeignLanguages/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ForeignLanguages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            string language = "unknown";
+
+            switch (name)
+            {
+                case "England":
+                case "USA":
+                    {
+                        language = "English";
+                        break;
+                    }
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    {
+                        language = "Spanish";
+                        break;
+                    }
+            }
+            Console.WriteLine(language);
+        }
+    }
+}
