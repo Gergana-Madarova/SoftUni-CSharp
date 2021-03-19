ddcf7226c1c1fca1540a1d948efd85fa8eed0a79
Gergana-Madarova <g.madarova@gmail.com>
2021-03-19 02:34:07 +0200

    Added L05SpecialNumbers

---
+++ Fundamentals CSharp/DataTypesAndVariables/L05SpecialNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace L05SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = 0;
+                bool special = false;
+                int currentNum = i;
+
+                while (currentNum != 0)
+                {
+                    sum += currentNum % 10;
+                    currentNum = currentNum / 10;
+                }           
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    special = true;
+                }
+                Console.WriteLine($"{i} -> {special}");
+            }
+        }
+    }
+}
