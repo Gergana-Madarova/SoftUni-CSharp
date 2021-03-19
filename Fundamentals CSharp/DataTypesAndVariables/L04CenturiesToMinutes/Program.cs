be84d76360797750028a1175b9d7962df1c99756
Gergana-Madarova <g.madarova@gmail.com>
2021-03-19 02:30:48 +0200

    Added L04CenturiesToMinutes

---
+++ Fundamentals CSharp/DataTypesAndVariables/L04CenturiesToMinutes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace L04CenturiesToMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centurie = int.Parse(Console.ReadLine());
+            int years = 100 * centurie;
+            int days = (int)(years * 365.2422);
+            long hours = 24 * (long)days;
+            long minutes = 60 * hours;
+
+            Console.WriteLine($"{centurie} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
