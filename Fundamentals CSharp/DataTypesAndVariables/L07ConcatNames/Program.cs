638684cbef69b6df9cc18adbbb3deb98e8ceda39
Gergana-Madarova <g.madarova@gmail.com>
2021-03-25 18:15:17 +0200

    Added L07ConcatNames

---
+++ Fundamentals CSharp/DataTypesAndVariables/L07ConcatNames/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace L07ConcatNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine(first + delimiter + second);
+        }
+    }
+}
