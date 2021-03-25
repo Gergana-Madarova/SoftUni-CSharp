cbff39827508f6a07d3b42513e199d3f2fdcff5a
Gergana-Madarova <g.madarova@gmail.com>
2021-03-25 18:20:09 +0200

    Added L09CharsToString

---
+++ Fundamentals CSharp/DataTypesAndVariables/L09CharsToString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace L09CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char tirth = char.Parse(Console.ReadLine());
+
+            string all = Convert.ToString(first) + Convert.ToString(second)+Convert.ToString(tirth);
+
+            Console.WriteLine(all);
+        }
+    }
+}
