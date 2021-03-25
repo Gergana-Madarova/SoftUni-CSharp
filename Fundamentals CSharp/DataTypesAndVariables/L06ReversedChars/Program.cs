903cd1bf1081e2687e068e93762b26347dadbebd
Gergana-Madarova <g.madarova@gmail.com>
2021-03-25 18:13:46 +0200

    Added L06ReversedChars

---
+++ Fundamentals CSharp/DataTypesAndVariables/L06ReversedChars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _06.ReversedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char tirth = char.Parse(Console.ReadLine());
+
+            Console.WriteLine(tirth + " " + second + " " + first);
+        }
+    }
+}
