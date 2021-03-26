77246c5c737c1f4e9ac3affe90706320c9ae733f
Gergana-Madarova <g.madarova@gmail.com>
2021-03-27 00:39:55 +0200

    Added L12RefactorSpecialNumbers

---
+++ Fundamentals CSharp/DataTypesAndVariables/L12RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12.RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool isSpecialNum = false;
+            for (int number = 1; number <= n; number++)
+            {
+                int currentNumber = number;
+                int sum = 0;
+                while (number > 0)
+                {
+                    sum += number % 10;
+                    number = number / 10;
+                }
+                isSpecialNum = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", currentNumber, isSpecialNum);
+                number = currentNumber;
+            }
+        }
+    }
+}
