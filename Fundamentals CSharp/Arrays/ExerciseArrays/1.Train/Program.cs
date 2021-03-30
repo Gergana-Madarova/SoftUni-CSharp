0a1be95a2ae354ce4747871a55c0c96d0ec87e10
Gergana-Madarova <g.madarova@gmail.com>
2021-03-31 01:59:05 +0300

    Added 01Train

---
+++ Fundamentals CSharp/Arrays/ExerciseArrays/1.Train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfWagons = int.Parse(Console.ReadLine());
+            int[] train = new int[countOfWagons];
+            int sum = 0;
+
+            for (int i = 0; i < train.Length; i++)
+            {
+                train[i] = int.Parse(Console.ReadLine());
+                sum += train[i];
+            }
+
+            Console.WriteLine(string.Join(" ", train));
+            Console.WriteLine(sum);
+        }
+    }
+}
