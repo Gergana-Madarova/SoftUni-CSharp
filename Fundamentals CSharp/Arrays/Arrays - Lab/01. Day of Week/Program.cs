dccd0eed944d7a8b88bdca091f81849008f0b0fa
Gergana-Madarova <g.madarova@gmail.com>
2021-03-30 20:57:45 +0300

    Added Array 01DayOfWeek

---
+++ Fundamentals CSharp/Arrays/Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfDay = int.Parse(Console.ReadLine());
+
+            string[] daysOfWeek =
+                { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            if (numOfDay > 0 && numOfDay < 8)
+            {
+                Console.WriteLine(daysOfWeek[numOfDay - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
