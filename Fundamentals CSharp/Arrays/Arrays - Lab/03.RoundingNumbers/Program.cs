c45f03e3e262cfac28bd0df69619c17a73391573
Gergana-Madarova <g.madarova@gmail.com>
2021-03-30 20:59:41 +0300

    Added 03RoundingNumbers

---
+++ Fundamentals CSharp/Arrays/Arrays - Lab/03.RoundingNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int roundedNumber = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{numbers[i]} => {roundedNumber}");
+            }
+        }
+    }
+}
