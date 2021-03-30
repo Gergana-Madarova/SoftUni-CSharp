9a17203628a4135171d06fe943aaf942e3675a31
Gergana-Madarova <g.madarova@gmail.com>
2021-03-30 21:01:02 +0300

    Added 04Reverse Array of Strings

---
+++ Fundamentals CSharp/Arrays/Arrays - Lab/4. Reverse Array of Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//using System.Buffers;
+using System.Linq;
+
+namespace _4._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split().ToArray();
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                string temp = arr[i];
+                arr[i] = arr[arr.Length - i - 1];
+                arr[arr.Length - 1 - i] = temp;
+            }
+
+            Console.WriteLine(string.Join(" ", arr));     
+        }
+    }
+}
