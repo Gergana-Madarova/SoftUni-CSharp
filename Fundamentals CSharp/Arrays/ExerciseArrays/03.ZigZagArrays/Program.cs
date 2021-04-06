90414cb73aa076a90dd18c15f49456ec4e2eecfd
Gergana-Madarova <g.madarova@gmail.com>
2021-04-06 13:37:48 +0300

    Added 03ZigZagArrays

---
+++ Fundamentals CSharp/Arrays/ExerciseArrays/03.ZigZagArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _03.ZigZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] result1 = new string[n];
+            string[] result2 = new string[n];
+
+            for (int i = 1; i <= n; i++)
+            {
+                string[] arr = Console.ReadLine().Split();
+
+                if (i % 2!=0)
+                {
+                    result1[i - 1] = arr[0];
+                    result2[i - 1] = arr[1];
+                }
+                else
+                {
+                    result1[i - 1] = arr[1];
+                    result2[i - 1] = arr[0];
+                }            
+            }
+            Console.WriteLine(string.Join(" ", result1));
+            Console.WriteLine(string.Join(" ", result2));
+        }
+    }
+}
