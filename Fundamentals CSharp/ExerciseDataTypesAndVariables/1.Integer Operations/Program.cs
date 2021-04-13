9d8190cb725f8303c4047b8ed79972aa8380364f
Gergana-Madarova <g.madarova@gmail.com>
2021-04-13 18:15:45 +0300

    Added 01.IntegerOperations

---
+++ Fundamentals CSharp/ExerciseDataTypesAndVariables/1.Integer Operations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1.Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long firstNum = long.Parse(Console.ReadLine());
+            long secondNum = long.Parse(Console.ReadLine());
+            long thirdNum = long.Parse(Console.ReadLine());
+            long fourthNum = long.Parse(Console.ReadLine());
+
+            long result = (int)((firstNum + secondNum) / thirdNum) * fourthNum;
+
+            Console.WriteLine(result);
+        }
+    }
+}
