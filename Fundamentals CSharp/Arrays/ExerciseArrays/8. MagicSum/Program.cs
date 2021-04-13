cfbb508acc8b39fef76d2ce93d95307e0abaaecf
Gergana-Madarova <g.madarova@gmail.com>
2021-04-13 17:18:17 +0300

    Added 08MagicSum

---
+++ Fundamentals CSharp/Arrays/ExerciseArrays/8. MagicSum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _8._MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputInt = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int givenNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputInt.Length; i++)
+            {
+                for (int j = i+1; j < inputInt.Length; j++)
+                {
+                    if (inputInt[i] + inputInt[j] == givenNumber)
+                    {
+                        Console.WriteLine($"{inputInt[i]} {inputInt[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
