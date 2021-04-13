649bc92bb2483b7d68f20ced5ac48790612e03e7
Gergana-Madarova <g.madarova@gmail.com>
2021-04-13 18:16:52 +0300

    Added 03.Elevator

---
+++ Fundamentals CSharp/ExerciseDataTypesAndVariables/03.Elevator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03.Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint numberOfPeople = uint.Parse(Console.ReadLine());
+            uint capacity = uint.Parse(Console.ReadLine());
+            uint courses = (uint)Math.Ceiling((double)numberOfPeople/capacity);
+
+            Console.WriteLine(courses);
+        }
+    }
+}
