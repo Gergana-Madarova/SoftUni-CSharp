d587536a9d25ce5d126d775724b5297232f1ca88
Gergana-Madarova <g.madarova@gmail.com>
2021-04-13 18:16:20 +0300

    Added 02.SumDigits

---
+++ Fundamentals CSharp/ExerciseDataTypesAndVariables/02.SumDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int currentNum = number;
+            int sum = 0;
+
+            while (currentNum > 0)
+            {
+                sum += (currentNum % 10);
+                currentNum = currentNum / 10;
+            }    
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
