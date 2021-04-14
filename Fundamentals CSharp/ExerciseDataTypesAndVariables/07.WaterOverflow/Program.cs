a7274d8a5cac22d29df07530be72a6b2980cdad7
Gergana-Madarova <g.madarova@gmail.com>
2021-04-14 16:05:04 +0300

    Added 07.WaterOverflow

---
+++ Fundamentals CSharp/ExerciseDataTypesAndVariables/07.WaterOverflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07.WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            byte litersInTank = 255;
+
+            for (byte i = 1; i <= n; i++)
+            {
+                ushort quantities = ushort.Parse(Console.ReadLine());
+
+                if ( litersInTank - quantities < 0)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    litersInTank -= (byte)quantities;
+                }
+            }
+            Console.WriteLine((byte)255 - litersInTank);
+        }
+    }
+}
