1b770cdc416a9b4e2044ac0abf62d2922041db06
Gergana-Madarova <g.madarova@gmail.com>
2021-04-14 16:03:23 +0300

    Added 05.PrintPartOfASCII

---
+++ Fundamentals CSharp/ExerciseDataTypesAndVariables/05.PrintPartOfASCII able/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05.PrintPartOfASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte charStart = byte.Parse(Console.ReadLine());
+            byte charEnd = byte.Parse(Console.ReadLine());
+
+            for (int i = charStart; i <= charEnd; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
