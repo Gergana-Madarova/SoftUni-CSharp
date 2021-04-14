11199504168e989a683774eae2a1fd8b5f9c858f
Gergana-Madarova <g.madarova@gmail.com>
2021-04-14 16:04:07 +0300

    Added 06TriplesOfLatinLetters

---
+++ Fundamentals CSharp/ExerciseDataTypesAndVariables/06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06.TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+
+            for (byte i = 97; i <= 96+ n; i++)
+            {
+                for (byte j = 97; j <= 96+n; j++)
+                {
+                    for (byte k = 97; k <= 96+n; k++)
+                    {
+                        Console.WriteLine($"{(char)i}{(char)j}{(char)k}");
+                    }
+                }
+            }      
+        }
+    }
+}
