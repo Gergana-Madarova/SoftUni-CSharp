cb217dab5948f34ede38d949be41f3c470964aa6
Gergana-Madarova <g.madarova@gmail.com>
2021-04-15 01:18:24 +0300

    Added 09.SpiceMustFlow

---
+++ Fundamentals CSharp/ExerciseDataTypesAndVariables/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _09.SpiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int curentYield = startingYield;
+            int daysOfWork = 0;
+            int totalAmountSpice = 0;
+
+            while (curentYield >= 100)
+            {
+                daysOfWork++;
+                totalAmountSpice += curentYield - 26;
+                curentYield = curentYield - 10;
+            }
+            if (totalAmountSpice >= 26)
+            {
+                totalAmountSpice = totalAmountSpice - 26;
+            }
+
+            Console.WriteLine(daysOfWork);
+            Console.WriteLine(totalAmountSpice);
+        }
+    }
+}
