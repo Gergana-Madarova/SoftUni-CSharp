9abe02df4d62e1d90cf5eab25a0abc4aa8e460b7
Gergana-Madarova <g.madarova@gmail.com>
2021-04-16 13:09:08 +0300

    Added L04PrintingTriangle

---
+++ Fundamentals CSharp/Methods/LabMethods/04. PrintingTriangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace _04._PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                int end = i;
+                PrintingTriangle(end);
+            }
+
+            for (int i = num - 1; i >= 1; i--)
+            {
+                int end = i;
+                PrintingTriangle(end);
+            }
+        }
+
+        private static void PrintingTriangle(int end)
+        {
+            for (int i = 1; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
