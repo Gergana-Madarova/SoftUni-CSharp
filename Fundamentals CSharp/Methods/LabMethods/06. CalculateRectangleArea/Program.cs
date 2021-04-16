c4e992bba684c3e3e2aa22d67a1120753e5f314f
Gergana-Madarova <g.madarova@gmail.com>
2021-04-16 13:12:11 +0300

    Added L06CalculateRectangleArea

---
+++ Fundamentals CSharp/Methods/LabMethods/06. CalculateRectangleArea/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06._CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double area = CalculateRectangleArea(width, height);
+
+            Console.WriteLine(area);
+        }
+
+        private static double CalculateRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
