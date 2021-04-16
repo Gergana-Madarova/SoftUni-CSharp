134d38a2acafa4621b580bfc385dbbfe8fb4ec8f
Gergana-Madarova <g.madarova@gmail.com>
2021-04-16 13:10:16 +0300

    Added L01SignOfIntegerNumbers

---
+++ Fundamentals CSharp/Methods/LabMethods/1.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1.SignOfIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            SignOfNumbers(number);
+        }
+
+        private static void SignOfNumbers(int number)
+        {
+            string output = string.Empty;
+            if (number > 0)
+            {
+                output = $"The number {number} is positive.";
+            }
+            else if (number < 0)
+            {
+                output = $"The number {number} is negative.";
+            }
+            else if (number == 0)
+            {
+                output = $"The number {number} is zero.";
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
