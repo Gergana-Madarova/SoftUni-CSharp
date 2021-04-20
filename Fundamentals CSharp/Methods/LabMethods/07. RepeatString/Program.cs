6db4bc956666e7c2bbf6c2f4ae51502a6a4dc3e4
Gergana-Madarova <g.madarova@gmail.com>
2021-04-20 12:46:12 +0300

    Added L07RepeatString

---
+++ Fundamentals CSharp/Methods/LabMethods/07. RepeatString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07._RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            string output = RepeatString(input, n);
+                     
+            Console.WriteLine(output);
+        }
+
+        private static string RepeatString(string input, int n)
+        {
+            string output = string.Empty;
+
+            for (int i = 1; i <= n; i++)
+            {
+                output += input;
+            }
+
+            return output;
+        }
+    }
+}
