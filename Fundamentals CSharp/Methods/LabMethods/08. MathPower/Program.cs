d3c0a4749f52c6b8105268ea48579c06d99027e1
Gergana-Madarova <g.madarova@gmail.com>
2021-04-20 12:47:12 +0300

    Added L08MathPower

---
+++ Fundamentals CSharp/Methods/LabMethods/08. MathPower/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _08._MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            double result = MathPower(number, power);
+
+            Console.WriteLine(result);
+        }
+
+        private static double MathPower(double number, int power)
+        {
+            return Math.Pow(number, power);
+        }
+    }
+}
