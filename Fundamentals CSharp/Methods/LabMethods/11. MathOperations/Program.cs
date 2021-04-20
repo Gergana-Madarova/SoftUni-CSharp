9b30a223fe768c48cb889a7f676fcd6f85e6fbd5
Gergana-Madarova <g.madarova@gmail.com>
2021-04-20 12:50:07 +0300

    L11MathOperations

---
+++ Fundamentals CSharp/Methods/LabMethods/11. MathOperations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _11._MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            char operators = char.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+            double result = MathOerator(firstNumber, secondNumber, operators);
+
+            Console.WriteLine(result);
+        }
+
+        private static double MathOerator(double firstNumber, double secondNumber, char operators)
+        {
+            double result = 0;
+
+            switch (operators)
+            {
+                case '/':
+                    {
+                        result = firstNumber / secondNumber;
+                        break;
+                    }
+                case '*':
+                    {
+                        result = firstNumber * secondNumber;
+                        break;
+                    }
+                case '+':
+                    {
+                        result = firstNumber + secondNumber;
+                        break;
+                    }
+                case '-':
+                    {
+                        result = firstNumber - secondNumber;
+                        break;
+                    }
+            }
+
+            return result;
+        }
+    }
+}
