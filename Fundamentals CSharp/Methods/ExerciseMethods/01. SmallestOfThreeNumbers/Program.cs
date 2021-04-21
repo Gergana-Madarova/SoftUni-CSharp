81eb6f40074993a64b76e1d9cce7968c6c7ed02f
Gergana-Madarova <g.madarova@gmail.com>
2021-04-22 00:11:19 +0300

    Added P01SmallestOfThreeNumbers

---
+++ Fundamentals CSharp/Methods/ExerciseMethods/01. SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01._SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int threeNumber = int.Parse(Console.ReadLine());
+
+            GetSmallestNumber(firstNumber, secondNumber, threeNumber);
+        }
+
+        private static void GetSmallestNumber(int firstNumber, int secondNumber, int threeNumber)
+        {
+            int result = 0;
+
+            if (firstNumber <= secondNumber && firstNumber <= threeNumber)
+            {
+                result = firstNumber;
+            }
+            else if (secondNumber <= firstNumber && secondNumber <= threeNumber)
+            {
+                result = secondNumber;
+            }
+            if (threeNumber <= secondNumber && firstNumber >= threeNumber)
+            {
+                result = threeNumber;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
