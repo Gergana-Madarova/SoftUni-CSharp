4f13a668516639dcdc78c0da689edba09018c81d
Gergana-Madarova <g.madarova@gmail.com>
2021-04-22 00:13:12 +0300

    Added P03CharactersInRange

---
+++ Fundamentals CSharp/Methods/ExerciseMethods/03. CharactersInRange/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _03._CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char character1 = char.Parse(Console.ReadLine());
+            char character2 = char.Parse(Console.ReadLine());
+
+            GetCharactersInRange(character1, character2);
+        }
+
+        private static void GetCharactersInRange(char character1, char character2)
+        {
+            if (character1 < character2)
+            {
+                for (char i = character1; i < character2; i++)
+                {
+                    if (i == character1)
+                    {
+                        continue;
+                    }
+                    Console.Write($"{i} ");
+                }
+            }
+            else if (character1 > character2)
+            {
+                for (char i = character2; i < character1; i++)
+                {
+                    if (i == character2)
+                    {
+                        continue;
+                    }
+                    Console.Write($"{i} ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
