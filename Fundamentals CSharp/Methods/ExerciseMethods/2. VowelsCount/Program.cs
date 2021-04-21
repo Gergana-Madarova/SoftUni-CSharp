a761e7203d975a596fa0397fa3537a222e5f370d
Gergana-Madarova <g.madarova@gmail.com>
2021-04-22 00:12:13 +0300

    Added P02VowelsCount

---
+++ Fundamentals CSharp/Methods/ExerciseMethods/2. VowelsCount/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace _2._VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+
+            int output = GetVowelsCount(input);
+
+            Console.WriteLine(output);
+        }
+
+        private static int GetVowelsCount(string input)
+        {
+            int count = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case 'a':
+                    case 'o':
+                    case 'e':
+                    case 'u':
+                    case 'i':
+                    case 'y':
+                        {
+                            count++;
+                        }
+                        break;
+                }
+            }
+
+            return count;
+        }
+    }
+}
