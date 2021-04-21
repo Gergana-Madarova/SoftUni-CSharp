3b9368e9caee774b3a051e909f1c6a1503a85bf7
Gergana-Madarova <g.madarova@gmail.com>
2021-04-22 00:14:17 +0300

    Added P05AddAndSubtract

---
+++ Fundamentals CSharp/Methods/ExerciseMethods/05. AddAndSubtract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int result = Sum(firstNumber, secondNumber);
+            Substract(result, thirdNumber);
+        }
+
+        private static int Sum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        private static void Substract(int result, int thirdNumber)
+        {
+            Console.WriteLine(result - thirdNumber);
+        }
+    }
+}
