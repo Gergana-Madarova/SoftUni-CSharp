dc8bb85602c987d66cabfcd965d00f35019f7051
Gergana-Madarova <g.madarova@gmail.com>
2021-04-23 17:12:06 +0300

    Added P01Ages

---
+++ Fundamentals CSharp/Basic Syntax, Conditional Statements and Loops/Ages/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string output = string.Empty;
+
+            if (age >= 66)
+            {
+                output = "elder";
+            }
+            else if (age >= 20)
+            {
+                output = "adult";
+            }
+            else if (age >= 14)
+            {
+                output = "teenager";
+            }
+            else if (age >= 3)
+            {
+                output = "child";
+            }
+            else if (age >= 0)
+            {
+                output = "baby";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
