2b83255ac0a54a77f8d012f46abc29543b7e6d0a
Gergana-Madarova <g.madarova@gmail.com>
2021-04-23 13:42:20 +0300

    P08FactorialDivision

---
+++ Fundamentals CSharp/Methods/ExerciseMethods/08. FactorialDivision/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08._FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong factorialFirstNum = Factorial(int.Parse(Console.ReadLine()));
+            ulong factorialSecondNum = Factorial(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"{factorialFirstNum * 1.0 / factorialSecondNum:f2}");
+        }
+
+        private static ulong Factorial(int num)
+        {
+            ulong result = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                result = result * (ulong)i;
+            }
+
+            return result;
+        }
+    }
+}
