817dfd787f3783f40c77ff9adb17045a113e8b80
Gergana-Madarova <g.madarova@gmail.com>
2021-04-23 13:41:12 +0300

    Added P07NxNMatrix

---
+++ Fundamentals CSharp/Methods/ExerciseMethods/07. NxNMatrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintMatrix(n);        
+        }
+
+        private static void PrintMatrix(int n)
+        {
+            for (int i  = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write($"{n} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
