4e00147df90b29aab20422a8c42381fa50e607fa
Gergana-Madarova <g.madarova@gmail.com>
2021-04-23 13:45:25 +0300

    Added P09PalindromeIntegers

---
+++ Fundamentals CSharp/Methods/ExerciseMethods/09. PalindromeIntegers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _09._PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+
+            while (input != "end")
+            {
+                string output = Palidrom(input).ToLower();
+                Console.WriteLine(output);
+                input = Console.ReadLine().ToLower();
+            }
+        }
+
+        private static string Palidrom(string input)
+        {
+            string isPalindrome = "true";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    isPalindrome = "false";
+                }
+            }
+
+            return isPalindrome;
+        }
+    }
+}
