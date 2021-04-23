c068c3b8c8f2c4c0394aaf990c845663e2932be9
Gergana-Madarova <g.madarova@gmail.com>
2021-04-23 13:46:44 +0300

    Added P10TopNumber

---
+++ Fundamentals CSharp/Methods/ExerciseMethods/10. TopNumber/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _10._TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 17; i <= number; i++)
+            {
+                GetTopNum(i);
+            }
+        }
+
+        private static void GetTopNum(int number)
+        {
+            int currNum = number;
+            bool haveOddDigit = false;
+
+            while (currNum != 0)
+            {
+                int currChifre = currNum % 10;
+
+                if (currChifre % 2 != 0)
+                {
+                    haveOddDigit = true;
+                    break;
+                }
+
+                currNum /= 10;
+            }
+
+            currNum = number;
+            int sumOfDigits = 0;
+            bool isDivisibleByEight = false;
+
+            while (currNum != 0)
+            {
+                sumOfDigits += currNum % 10;
+                currNum /= 10;
+            }
+
+            if (sumOfDigits % 8 == 0)
+            {
+                isDivisibleByEight = true;
+            }
+
+            if (isDivisibleByEight && haveOddDigit)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
