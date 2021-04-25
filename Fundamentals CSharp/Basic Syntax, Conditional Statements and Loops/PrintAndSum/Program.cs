1d1f894ab33dc18a5cd21f590cc5816099b4627e
Gergana-Madarova <g.madarova@gmail.com>
2021-04-25 18:33:35 +0300

    Added P04PrintAndSum

---
+++ Fundamentals CSharp/Basic Syntax, Conditional Statements and Loops/PrintAndSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                if (i == endNum)
+                {
+                    Console.Write(i);
+                    sum += i;
+                    break;
+                }
+                Console.Write(i + " ");
+                sum += i;
+            }
+
+            Console.WriteLine($"\nSum: {sum}");
+        }
+    }
+}
