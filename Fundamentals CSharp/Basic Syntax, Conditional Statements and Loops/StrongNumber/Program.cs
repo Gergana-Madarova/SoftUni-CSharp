7f28bd177910ee56379041476ba9fb276710e59d
Gergana-Madarova <g.madarova@gmail.com>
2021-04-25 18:34:52 +0300

    Added P06StrongNumber

---
+++ Fundamentals CSharp/Basic Syntax, Conditional Statements and Loops/StrongNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i <= num.Length - 1; i++)
+            {
+                int currNum = (int)char.GetNumericValue(num[i]);
+                int curFactorial = 1;
+
+                for (int j = 1; j <= currNum; j++)
+                {
+                    curFactorial *= j;
+                }
+                sum += curFactorial;
+            }
+
+            if (sum == int.Parse(num))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
