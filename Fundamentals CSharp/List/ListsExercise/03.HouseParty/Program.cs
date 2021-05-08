e180abbff2d63bbda3d77e5c2e9aff8a3ae79404
Gergana-Madarova <g.madarova@gmail.com>
2021-05-09 02:01:33 +0300

    Added P03HouseParty

---
+++ Fundamentals CSharp/List/ListsExercise/03.HouseParty/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfComands = int.Parse(Console.ReadLine());
+
+            List<string> guests = new List<string>(numberOfComands);
+
+            for (int i = 1; i <= numberOfComands; i++)
+            {
+                string[] comands = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (comands[2] == "going!")
+                {
+                    if (guests.Contains(comands[0]))
+                    {
+                        Console.WriteLine($"{comands[0]} is already in the list!");
+                    }
+                    else
+                    {
+                        guests.Add(comands[0]);
+                    }
+                }
+                else if (comands[2] == "not")
+                {
+                    if (guests.Contains(comands[0]))
+                    {
+                        guests.Remove(comands[0]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{comands[0]} is not in the list!");
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine, guests));
+        }
+    }
+}
