b6c2d8704fcefc8249b348d171d08b5fd50f3bce
Gergana-Madarova <g.madarova@gmail.com>
2021-05-10 13:55:32 +0300

    Added P07AppendArrays

---
+++ Fundamentals CSharp/List/ListsExercise/P07AppendArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputList = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .Where(s => !string.IsNullOrWhiteSpace(s))  //без този ред -> 75/100 точки (без един тест в judge)
+                .Reverse()
+                .ToList();
+
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                List<string> outputList = inputList[i].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                Console.Write(string.Join(' ', outputList));
+                if (i != inputList.Count - 1)
+                {
+                    Console.Write(' ');
+                }
+            }
+        }
+    }
+}
