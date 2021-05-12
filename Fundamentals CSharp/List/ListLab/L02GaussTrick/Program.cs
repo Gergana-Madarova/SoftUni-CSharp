26499e9756934f0b5026cc8b3c8c4daee2ec1735
Gergana-Madarova <g.madarova@gmail.com>
2021-05-13 02:17:24 +0300

    Added L02GaussTrick

---
+++ Fundamentals CSharp/List/ListLab/L02GaussTrick/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L02GaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            //double sum = 0.0;
+            int numbersCount = numbers.Count;
+
+            List<double> newNumbers = new List<double>((numbersCount + 1) / 2);
+
+            for (int i = 0; i < (numbersCount + 1) / 2; i++)
+            {
+                if (numbersCount % 2 != 0 && i == numbersCount / 2)
+                {
+                    newNumbers.Add(numbers[0 + i]);
+                    break;
+                }
+
+                double sum = numbers[0 + i] + numbers[numbersCount - 1 - i];
+                newNumbers.Add(sum);
+            }
+
+            Console.WriteLine(string.Join(" ", newNumbers));
+        }
+    }
+}
