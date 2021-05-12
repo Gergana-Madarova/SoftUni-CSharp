fb33954fc57c964594c5743ee748b5b4de07aaa8
Gergana-Madarova <g.madarova@gmail.com>
2021-05-12 04:14:11 +0300

    update DepositCalculator

--- Programming-Basics-CSharp/FirstStepsInCodingLab/DepositCalculator/Program.cs
+++ Programming-Basics-CSharp/FirstStepsInCodingLab/DepositCalculator/Program.cs
@@ -7,12 +7,12 @@ namespace DepositCalculator
     {
         static void Main(string[] args)
         {
-            double Deposit = double.Parse(Console.ReadLine());
-            int Period = int.Parse(Console.ReadLine());
-            double AnnualInterest = double.Parse(Console.ReadLine());
+            double deposit = double.Parse(Console.ReadLine());
+            int period = int.Parse(Console.ReadLine());
+            double annualInterest = double.Parse(Console.ReadLine());
 
             //сума = депозирана сума  + срок на депозита * ((депозирана сума * годишен лихвен процент ) / 12)
-            double sum = Deposit + Period * ((Deposit * AnnualInterest * 0.01) / 12);
+            double sum = deposit + period * ((deposit * annualInterest * 0.01) / 12);
 
             Console.WriteLine(sum);
         }
