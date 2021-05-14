9c0f7d02f90b2dd95471d38b95018f70e4f100f5
Gergana-Madarova <g.madarova@gmail.com>
2021-05-14 22:31:21 +0300

    Added L06ListManipulationBasics

---
+++ Fundamentals CSharp/List/ListLab/L06ListManipulationBasics/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L06ListManipulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command.ToLower() != "end")
+            {
+                string[] tokens = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                switch (tokens[0])
+                {
+                    case "Add":
+                        numbers.Add(int.Parse(tokens[1]));
+                        break;
+                    case "Remove":
+                        numbers.Remove(int.Parse(tokens[1]));
+                        break;
+                    case "RemoveAt":
+                        numbers.RemoveAt(int.Parse(tokens[1]));
+                        break;
+                    case "Insert":
+                        numbers.Insert(int.Parse(tokens[2]), int.Parse(tokens[1]));
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
