80e1b5338832bfe00eb2d0e28173023e2979e9ff
Gergana-Madarova <g.madarova@gmail.com>
2021-05-14 18:09:55 +0300

    Added L05RemoveNegativesAndReverse

---
+++ Fundamentals CSharp/List/ListLab/L05RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L05RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+
+            //numbers.RemoveAll(n => n < 0);
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.Remove(numbers[i]);
+                    i--;
+                }
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(' ', numbers));
+            }
+        }
+    }
+}
