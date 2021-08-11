a0b7e40c96717e80fdfadd0395a2a9853ebd45b4
Gergana-Madarova <g.madarova@gmail.com>
2021-08-11 15:52:30 +0300

    Added L04Largest3Numbers C#

---
+++ Fundamentals CSharp/AssociativeArrays/L04Largest3Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace L04Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderByDescending(n => n)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers.Take(3)));
+        }
+    }
+}
