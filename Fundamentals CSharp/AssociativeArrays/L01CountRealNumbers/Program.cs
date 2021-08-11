32523e3d22f47079b5fa6e8c5e11901f9646671a
Gergana-Madarova <g.madarova@gmail.com>
2021-08-11 15:49:08 +0300

    Added L01CountRealNumbers C#

---
+++ Fundamentals CSharp/AssociativeArrays/L01CountRealNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace L01CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (double num in numbers)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts.Add(num, 1);
+                }
+            }
+
+            foreach (var num in counts)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+        }
+    }
+}
