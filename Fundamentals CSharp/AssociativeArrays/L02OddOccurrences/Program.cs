31e300ebba98d37f7103feb090518248dd088440
Gergana-Madarova <g.madarova@gmail.com>
2021-08-11 15:50:25 +0300

    Added L02OddOccurrences

---
+++ Fundamentals CSharp/AssociativeArrays/L02OddOccurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L02OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in input)
+            {
+                string wordInLowerCase = word.ToLower();
+                if (counts.ContainsKey(wordInLowerCase))
+                {
+                    counts[wordInLowerCase]++;
+                }
+                else
+                {
+                    counts.Add(wordInLowerCase, 1);
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
